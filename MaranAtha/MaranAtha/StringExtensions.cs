73a3d89ddd5a86b9cd08d2538420d653c91d64e4
Angelos <angelos.m.reda@gmail.com>
2018-03-24 22:27:50 +0200

    Adding descriptions for all methods at StringExtensions class

--- MaranAtha/MaranAtha/StringExtensions.cs
+++ MaranAtha/MaranAtha/StringExtensions.cs
@@ -4,11 +4,21 @@ namespace MaranAtha
 {
     public static class StringExtensions
     {
+        /// <summary>
+        /// Removes all leading and trailing white-space characters and converts the in-between multiple white-space into only one space from the current String object.
+        /// </summary>
+        /// <param name="str">The string to be trimmed</param>
+        /// <returns>The trimmed string</returns>
         public static string FullTrim(this string str)
         {
             return Regex.Replace(str.TrimStart().TrimEnd().Trim(), @"\s+", " ");
         }
 
+        /// <summary>
+        /// Indicates whether a specified string is null, empty (System.String.Empty ("")), or consists only of white-space characters.
+        /// </summary>
+        /// <param name="str">The string to be tested.</param>
+        /// <returns>true if the str parameter is null or empty (System.String.Empty ("")), or if str consists exclusively of white-space characters.</returns>
         public static bool IsNullOrEmptyOrWhiteSpace(this string str)
         {
             return string.IsNullOrEmpty(str) || string.IsNullOrWhiteSpace(str);
